9a02cf6b602eb36492562fd76c56af83ff20678f
lupengfei <fei879035762@163.com>
2019-01-31 23:42:57 +0800

    aa

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongodb=require('mongodb').MongoClient;
-var db_str="mongodb://127.0.0.1:27017/"
+var db_str="mongodb://127.0.0.1:27017"
 var ObjectId=require('mongodb').ObjectId;
 var async=require('async')
 /* GET home page. */
@@ -11,15 +11,57 @@ router.get('/', function(req, res, next) {
 
 
 //注册
-router.get('/regist',(req,res)=>{
-	res.render('regist',{})
+router.get("/register",(req,res)=>{
+	res.render("register",{});
 })
 
 //登陆
-router.get('/login',(req,res)=>{
+router.get("/login",(req,res)=>{
 	
-	res.render('login',{})
+	res.render("login",{})
 	
 })
+
+//管理员
+router.get('/root',(req,res)=>{
+	mongodb.connect(db_str,(err,database)=>{
+		database.collection('user',(err,coll)=>{
+			
+//			页码
+			var pageNo=req.query.pageNo;
+			pageNo=pageNo?pageNo:1;
+//			每页展示的条数
+			var size=3;
+//			总页数
+			var page=0;
+//			总条数
+			var totals=0;
+			
+			async.series([
+				function(callback){
+					coll.find({}).toArray((err,data)=>{
+						
+						totals=data.length;
+						page=Math.ceil(totals/size)
+//						上一页下一页
+						pageNo=pageNo<1?1:pageNo;
+						pageNo=pageNo>page?page:pageNo;	
+					})
+					callback(null,'')
+				},
+				function(callback){
+					coll.find({}).sort({_id:-1}).limit(size).skip((pageNo-1)*size).toArray((err,data)=>{
+						callback(null,data)
+					})	
+				}
+			],function(err,data){
+//				data  [ '',data ]
+				res.render('root',{data:data[1],pageNo:pageNo,page:page,totals:totals,name: req.session.name})
+				database.close()
+			})
+		})
+	})
+})
+
 
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,15 +2,56 @@ var express = require('express');
 var router = express.Router();
 var mongodb=require('mongodb').MongoClient;
 var ObjectId=require('mongodb').ObjectId;
-var db_str="mongodb://127.0.0.1:27017/"
+var db_str="mongodb://127.0.0.1:27017"
 var upload=require('./upload')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.post("/register",(req,res)=>{
+	var data = req.body;
+	var username = data.username;
+	//查询是否重名
+	mongodb.connect(db_str,{ useNewUrlParser: true },(err,db)=>{
+		if(err) throw err;
+		var dbase = db.db("mydb");
+		var cols = dbase.collection("users");
+		cols.find({username:username}).toArray((err,result)=>{
+			if(result.length != 0){
+				res.send("0");
+			}else{
+				cols.insertOne(data,(err,result)=>{
+					res.send("1");
+				})
+			}
+			db.close();
+		})
+	})
+});
+//登录接口
+router.post("/login",(req,res)=>{
+	var data = req.body;
+	mongodb.connect(db_str,{ useNewUrlParser: true },(err,db)=>{
+		var dbase = db.db("mydb");
+		var cols = dbase.collection("users");
+		cols.find(data).toArray((err,result)=>{
+			if(result.length==0){
+				res.send("0");
+			}else{
+				//存session 默认存在内存里
+				//console.log(req.session);
+				req.session.username = data.username;
+				res.send("1");
+			}
+		})
+		db.close();
+	})
+})
+
 //注册
-router.post('/regist',(req,res)=>{
+/*router.post('/register',(req,res)=>{
 	mongodb.connect(db_str,(err,database)=>{
 		database.collection('user',(err,coll)=>{
 			coll.find({username:req.body.username}).toArray((err,data)=>{
@@ -47,6 +88,6 @@ router.post('/login',(req,res)=>{
 			})
 		})
 	})
-})
+})*/
 
 module.exports = router;
